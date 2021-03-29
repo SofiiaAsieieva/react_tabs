e716ddb0fd4b1006134dbe3574ed216f6cd0a88c
Sofiia Asieieva <sofiia.asieieva@gmail.com>
2021-03-29 22:08:36 +0300

    Updates

---
+++ precache-manifest.1295e499e6f2d028f249914b190febce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f1079168316b272da83fe5afa47b91d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7569183fe99aa5b78d84",
+    "url": "./static/css/main.70ddb5d4.chunk.css"
+  },
+  {
+    "revision": "4b964fc25ecc0b658447",
+    "url": "./static/js/2.18166693.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.18166693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7569183fe99aa5b78d84",
+    "url": "./static/js/main.ae85ff6e.chunk.js"
+  },
+  {
+    "revision": "53d1114eedfc0eaca354",
+    "url": "./static/js/runtime-main.a6df9e19.js"
+  }
+]);
